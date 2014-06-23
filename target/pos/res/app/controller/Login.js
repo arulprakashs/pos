38ac61d2e77bc3ff068181191fa8d714088d7cf7
arulprakashs <arulprakashst@gmail.com>
2014-06-23 09:21:38 +0800

    deleted target

--- target/pos/res/app/controller/Login.js
+++
@@ -1,7 +0,0 @@
-Ext.define('pos.controller.Login', {
-    extend: 'Ext.app.Controller',
- 
-    models: ['User'],
-    stores: ['Users'],
-    views: ['Display']
-});
--- target/pos/res/app/model/User.js
+++
@@ -1,4 +0,0 @@
-Ext.define('pos.model.User', {
-    extend: 'Ext.data.Model',
-    fields : ['id','name']
-});
